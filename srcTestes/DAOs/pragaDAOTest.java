fba6e2c93bd7b2af8b8f80ac983595363f7bdeb7
Alexandre <thekongotf@gmail.com>
2021-04-09 00:09:17 -0300

    Test Results XML

--- srcTestes/DAOs/pragaDAOTest.java
+++ srcTestes/DAOs/pragaDAOTest.java
@@ -1,13 +1,12 @@
 package DAOs;
 
-import Entities.Local;
+
 import Entities.Praga;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
